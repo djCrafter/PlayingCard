55cf432524f72268d1bc0ae357f7d4577f8ab796
Crafter <godhedin2000@gmail.com>
2019-04-09 22:12:44 +0300

    Commit message

---
+++ PlayingCard/PlayingCardDeck.swift
@@ -0,0 +1,46 @@
+//
+//  PlayingCardDeck.swift
+//  PlayingCard
+//
+//  Created by Crafter on 4/9/19.
+//  Copyright © 2019 Crafter. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCardDeck {
+   
+   private(set) var cards = [PlayingCard]()
+    
+    init(){
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                 cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        }
+        else{
+            return nil
+        }
+    }
+    
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        }
+        else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        }
+        else{
+            return 0
+        }
+    }
+}
